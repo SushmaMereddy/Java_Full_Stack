23fb2be59fa81aa03d98d4af43f2b35b563c2f00
Sushma Reddy <sushmamr231@gmail.com>
2023-06-21 12:21:53 -0500

    variable declaration

---
+++ Variable.java
@@ -0,0 +1,15 @@
+class Variable{
+	public static void main(String[] args){
+		int a=4; 
+		double b=5.5;
+		char c='q';
+		boolean d=true;
+			
+	System.out.println("Integer: "+a);
+	System.out.println("Double: "+b);
+	System.out.println("Character: "+c);
+	System.out.println("Boolean: "+d);
+	
+	
+	}
+}
