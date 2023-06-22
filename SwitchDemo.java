49c56e08d292141d95eac9334588b8e7ab063968
Sushma Reddy <sushmamr231@gmail.com>
2023-06-22 01:09:39 -0500

    Switch case demo

---
+++ SwitchDemo.java
@@ -0,0 +1,15 @@
+class SwitchDemo{
+	
+	public static void main(String[] args){
+		int ch=4;
+		switch(ch){
+			case 1: System.out.println("First Choice");
+					break;
+			case 2: System.out.println("Second Choice");
+					break;
+			case 3: System.out.println("Third Choice");
+					break;
+			default: System.out.println("Choice Over");
+		}
+	}
+}
