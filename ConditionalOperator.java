bbb0aa1830635d4e73a9cc4ff3e7da15aadddab1
Sushma Reddy <sushmamr231@gmail.com>
2023-06-21 22:35:19 -0500

    ConditionalOperator

---
+++ ConditionalOperator.java
@@ -0,0 +1,9 @@
+class ConditionalOperator{
+	public static void main(String[] args){
+		String res=5<6 ? "Hi" : "Bye";
+		System.out.println(res);
+		int x=5;
+		String res1=x%2==0 ? "Even no" :"Odd no";
+		System.out.println(res1);
+	}
+}
