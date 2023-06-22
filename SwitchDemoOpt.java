b70438c7aa069233c3d272307568be15907e8a90
Sushma Reddy <sushmamr231@gmail.com>
2023-06-22 01:11:30 -0500

    SwitchCase opt src

---
+++ SwitchDemoOpt.java
@@ -0,0 +1,19 @@
+class SwitchDemoOpt{
+	
+	public static void main(String[] args){
+		char opt='S';
+		switch(opt){
+			case 'I': System.out.println("IPHONE");
+						break;
+			case 'S': System.out.println("SAMSUNG");
+						break;
+			case 'N': System.out.println("NOKIA");
+						break;
+			case 'B': System.out.println("BLACKBERRY");
+						break;
+			default : System.out.println("NO MOBILE OPTION");
+						
+		}
+		
+	}
+}
