3aaf86215cea0d354c24b78c9879e674bc23f10e
Sushma Reddy <sushmamr231@gmail.com>
2023-07-12 13:00:13 -0500

    Encapsulation

---
+++ OOPS/Encapsulation/MainApp.java
@@ -0,0 +1,14 @@
+package com.practice.oops.Encapsulation;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		
+		Organization org=new Organization();		
+		org.displayEmployee();
+		System.out.println(org.emp1);
+		
+		
+	}
+
+}
---
+++ OOPS/Encapsulation/Organization.java
@@ -0,0 +1,28 @@
+package com.practice.oops.Encapsulation;
+
+public class Organization {
+	
+	public String emp1="Joe Tribyani";
+	String emp2="Chandler";
+	String emp3="Rachel";
+	String emp4="Monica";
+	String emp5="Phebe";
+	String emp6="Ross";
+	
+	public void displayEmployee() {
+		System.out.println(emp1);
+		System.out.println(emp2);
+		System.out.println(emp3);
+		System.out.println(emp4);
+		System.out.println(emp5);
+		System.out.println(emp6);
+	}
+	
+	/*public static void main(String[] args) {
+		
+		Organization org=new Organization();
+		org.displayEmployee();
+		
+	}*/
+
+}
